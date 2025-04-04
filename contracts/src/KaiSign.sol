eb5ba237d436af958fa2cc1e0c37c1b35fa2e43d
Edmund Edgar <ed@socialminds.jp>
2025-04-04 22:14:37 +0800

    Basic repo entry

--- contracts/src/KaiSign.sol
+++ contracts/src/KaiSign.sol
@@ -2,13 +2,35 @@
 pragma solidity ^0.8.13;
 
 contract KaiSign {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    enum Status {
+        Submitted,
+        Accepted,
+        Rejected
     }
 
-    function increment() public {
-        number++;
+    struct ERC7730Spec {
+        uint64 createdTimestamp;
+        Status status;
+        string ipfs;
+    }
+
+    mapping(bytes32 => ERC7730Spec) public specs;
+
+    function createSpec(string calldata ipfs) external {
+        bytes32 specID = keccak256(bytes(ipfs));
+        require(specs[specID].createdTimestamp == 0, "Already proposed");
+        specs[specID] = ERC7730Spec(uint64(block.timestamp), Status.Submitted, ipfs);
+    }
+
+    function getCreatedTimestamp(bytes32 id) external returns (uint64) {
+        return specs[id].createdTimestamp;
+    }
+
+    function getStatus(bytes32 id) external returns (Status) {
+        return specs[id].status;
+    }
+
+    function getIPFS(bytes32 id) external returns (string memory) {
+        return specs[id].ipfs;
     }
 }
---
+++ contracts/test/KaiSign.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {KaiSign} from "../src/KaiSign.sol";
+
+contract KaiSignTest is Test {
+    KaiSign public kaisign;
+
+    function setUp() public {
+        kaisign = new KaiSign();
+        string memory ipfs_hash = "Qmbdr7gTLeWZYLVHALapahbcDQtsGDvoYRcVS73QYGnTmk";
+        kaisign.createSpec(ipfs_hash);
+    }
+
+    function test_submitEntryCreatesEntry() public {
+        string memory ipfs_hash = "Qmbdr7gTLeWZYLVHALapahbcDQtsGDvoYRcVS73QYGnTmk";
+        bytes32 id = keccak256(bytes(ipfs_hash));
+        assertGt(kaisign.getCreatedTimestamp(id), 0);
+    }
+
+    function test_submitDuplicateEntryFails() public {
+        string memory ipfs_hash = "Qmbdr7gTLeWZYLVHALapahbcDQtsGDvoYRcVS73QYGnTmk";
+        bytes32 id = keccak256(bytes(ipfs_hash));
+        vm.expectRevert();
+        kaisign.createSpec(ipfs_hash);
+    }
+}
