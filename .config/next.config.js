dfd8a9607befe6d963f5ee0062438be262697303
Nathanael Richard Ha Hanes <natrichardha@gmail.com>
2025-04-04 22:14:24 +0800

    configuration files

---
+++ .config/next.config.js
@@ -0,0 +1,40 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+
+/** @type {import("next").NextConfig} */
+const config = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/py/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/py/:path*"
+            : "/api/",
+      },
+      {
+        source: "/docs",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/py/docs"
+            : "/api/py/docs",
+      },
+      {
+        source: "/openapi.json",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/py/openapi.json"
+            : "/api/py/openapi.json",
+      },
+      {
+        source: "/api/trpc/:path*",
+        destination: "/api/trpc/:path*",
+      },
+    ];
+  },
+};
+
+export default config;
