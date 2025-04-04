ca9fbc501335f98e5b0e18874da3c3ca09544e84
Edmund Edgar <ed@socialminds.jp>
2025-04-05 07:24:07 +0800

    Break out deploy params

--- contracts/script/DeployKaiSign.s.sol
+++ contracts/script/DeployKaiSign.s.sol
@@ -2,24 +2,15 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {DeployParam} from "./DeployParam.sol";
 import {KaiSign} from "../src/KaiSign.sol";
 
-contract KaiSignScript is Script {
-    struct DeployParameters {
-        address arbitrator;
-        uint256 minBond;
-        address realityETH;
-        uint32 timeout;
-    }
-
-    string json = vm.readFile(string.concat(vm.projectRoot(), "/script/input/params.json"));
-    bytes data = vm.parseJson(json);
-    DeployParameters params = abi.decode(data, (DeployParameters));
-
+contract KaiSignScript is Script, DeployParam {
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
+        DeployParams memory params = getDeployParams();
         new KaiSign(params.realityETH, params.arbitrator, params.minBond, params.timeout);
         vm.stopBroadcast();
     }
